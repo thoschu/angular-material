eda2e73ce6f7b88e7782f806605f9bca66ef1b83
Tom S. <thoschulte@googlemail.com>
2024-03-25 19:45:06 +0100

    feat: add more logic and so on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSliderModule } from '@angular/material/slider';
+import { BreakpointObserver, Breakpoints, BreakpointState, MediaMatcher } from '@angular/cdk/layout';
+import {filter, Observable} from 'rxjs';
+
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSliderModule],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   protected readonly title: string = 'Tom S.';
 
-  constructor() {}
+  constructor(
+    private readonly breakpointObserver: BreakpointObserver,
+    private readonly mediaMatcher: MediaMatcher
+  ) {
+    const mediaQueryList: MediaQueryList = mediaMatcher.matchMedia(Breakpoints.Tablet);
+    console.log(mediaQueryList);
+  }
 
-  private open(): void {}
+  ngOnInit(): void {
+    const layoutChanges: Observable<BreakpointState> = this.breakpointObserver.observe([
+      // '(orientation: portrait)',
+      // '(orientation: landscape)',
+      // Breakpoints.HandsetLandscape,
+      // Breakpoints.HandsetPortrait,
+      Breakpoints.Handset,
+      // Breakpoints.TabletLandscape,
+      // Breakpoints.TabletPortrait,
+      // Breakpoints.Tablet,
+      // Breakpoints.WebLandscape,
+      // Breakpoints.WebPortrait,
+      // Breakpoints.Web,
+      // Breakpoints.XSmall,
+      // Breakpoints.Small,
+      // Breakpoints.Medium,
+      // Breakpoints.Large,
+      // Breakpoints.XLarge,
+      // '(max-width: 959px)',
+    ]);
+
+    layoutChanges
+      // .pipe(filter((breakpointState: BreakpointState) => breakpointState.matches))
+      .subscribe((breakpointState: BreakpointState): void => {
+        console.log(breakpointState);
+      });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+export const routes: Routes = [
+  { path: '', component: FirstComponent },
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+];
