2b9fd3572aebe704ea1c4acea66a62646cd0396b
Tom S. <thoschulte@googlemail.com>
2024-03-26 13:08:55 +0100

    feat: add images

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './first.component.html',
   styleUrl: './first.component.scss'
 })
--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-second',
   standalone: true,
-  imports: [],
+    imports: [
+        NgOptimizedImage
+    ],
   templateUrl: './second.component.html',
   styleUrl: './second.component.scss'
 })
