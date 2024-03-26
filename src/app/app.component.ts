3287a0d4909743bd4e7e4a0c11a9ee7ca895e140
Tom S. <thoschulte@googlemail.com>
2024-03-26 15:33:28 +0100

    feat: simple navigation system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSliderModule } from '@angular/material/slider';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { FormBuilder } from '@angular/forms';
+import {MatListItem, MatNavList} from "@angular/material/list";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSliderModule],
+  imports: [
+    MatSidenavModule, MatToolbarModule, MatIconModule,
+    MatIconButton, MatNavList, MatListItem,
+    RouterLink, RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   protected readonly title: string = 'Tom S.';
-  value = 0;
+  options = this._formBuilder.group({
+    bottom: 0,
+    fixed: false,
+    top: 0,
+  });
+
+
+  constructor(private readonly _formBuilder: FormBuilder) {
+  }
 }
