3f2d3064c347ed61fffee2db4d9c640a2506e141
Tom S. <thoschulte@googlemail.com>
2024-03-27 12:22:01 +0100

    feat: add mat-tab etc.

---
+++ src/app/one/one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one',
+  standalone: true,
+  imports: [],
+  templateUrl: './one.component.html',
+  styleUrl: './one.component.scss'
+})
+export class OneComponent {
+
+}
---
+++ src/app/two/two.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two',
+  standalone: true,
+  imports: [],
+  templateUrl: './two.component.html',
+  styleUrl: './two.component.scss'
+})
+export class TwoComponent {
+
+}
