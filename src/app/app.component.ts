9019509a4bbab25ec75959bee7bbe2fd2e7c69b5
Tom S. <thoschulte@googlemail.com>
2024-04-06 13:27:19 +0200

    feat: add building blocks, preparation for next subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatDivider} from "@angular/material/divider";
+import {MatAnchor, MatButton, MatFabButton, MatIconButton, MatMiniFabButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSliderModule],
+  imports: [RouterOutlet, MatSliderModule, MatDivider, MatAnchor, MatButton, MatIcon, MatIconButton, MatFabButton, MatMiniFabButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
