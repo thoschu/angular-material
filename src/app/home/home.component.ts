415a6cb1b06a40cf514fdd06a24cb4cee0e383df
Tom S. <thoschulte@googlemail.com>
2024-04-15 17:01:49 +0200

    refactor: update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,16 +5,17 @@ import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuItem } from '@angular/material/menu';
 import { TranslocoDirective } from '@jsverse/transloco';
+import {CdkFixedSizeVirtualScroll, CdkVirtualScrollViewport} from "@angular/cdk/scrolling";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    NgOptimizedImage,
-    MatCard, MatCardContent,
-    MatCardHeader, MatDivider, MatIcon,
-    MatMenuItem, TranslocoDirective
-  ],
+    imports: [
+        NgOptimizedImage,
+        MatCard, MatCardContent,
+        MatCardHeader, MatDivider, MatIcon,
+        MatMenuItem, TranslocoDirective, CdkFixedSizeVirtualScroll, CdkVirtualScrollViewport
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,40 +1,34 @@
 import { Component, inject, OnInit } from '@angular/core';
-import {AsyncPipe, NgOptimizedImage} from '@angular/common';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { MatIcon } from '@angular/material/icon';
 import { MatTab, MatTabGroup, MatTabLabel } from '@angular/material/tabs';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDivider } from '@angular/material/divider';
-import {BreakpointObserver, Breakpoints, BreakpointState} from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { TranslocoDirective } from '@jsverse/transloco';
-import {Observable} from "rxjs";
+import {map, Observable} from 'rxjs';
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
   imports: [
-    NgOptimizedImage,
-    ScrollingModule,
-    MatIcon,
-    MatTab,
-    MatTabGroup,
-    MatTabLabel,
-    MatExpansionModule,
-    MatDivider,
-    TranslocoDirective,
-    AsyncPipe
+    NgOptimizedImage, ScrollingModule,
+    MatTab, MatTabGroup,
+    MatTabLabel, MatExpansionModule,
+    MatDivider, TranslocoDirective, AsyncPipe, MatTooltip
   ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent implements OnInit {
   protected readonly breakpointObserver: BreakpointObserver = inject<BreakpointObserver>(BreakpointObserver);
-  protected readonly layoutChanges: Observable<BreakpointState>;
+  protected readonly layoutChanges: Observable<boolean>;
 
   constructor() {
     this.layoutChanges = this.breakpointObserver.observe([
-      Breakpoints.HandsetPortrait
-    ]);
+      Breakpoints.Handset
+    ]).pipe(map((res: BreakpointState) => res.matches));
   }
 
   ngOnInit(): void {
