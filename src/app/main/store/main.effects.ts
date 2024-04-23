8c2f7673ede908bf005f5909303560306a94dd07
Tom S. <thoschulte@googlemail.com>
2024-04-23 14:34:53 +0200

    refactor: update

--- src/app/main/store/main.effects.ts
+++ src/app/main/store/main.effects.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, CreateEffectMetadata, ofType } from '@ngrx/effects';
 import { Action, Store } from '@ngrx/store';
-import { map, of, switchMap, tap } from 'rxjs';
+import { TypedAction } from '@ngrx/store/src/models';
+import { combineLatest, map, Observable, switchMap, take, tap } from 'rxjs';
+import { assoc, head, last } from 'ramda';
 
 import { loadIpAction, setIpAction, setTechnologyAction } from './main.actions';
 import { AppState } from '../../app.store';
 
 @Injectable()
 export class MainEffects {
-  private readonly setTechnologyDisabledEffect$ = createEffect(() => this.actions$.pipe(
+  private readonly setTechnologyDisabledEffect$: Observable<Record<"disabled", boolean>> & CreateEffectMetadata = createEffect(() => this.actions$.pipe(
     ofType(setTechnologyAction),
     tap((action: Record<'disabled', boolean>): void => {
       // console.log(action);
@@ -19,11 +21,11 @@ export class MainEffects {
   private readonly setImprintIpEffect$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(loadIpAction),
-      switchMap(() => this.httpClient.get('https://checkip.amazonaws.com/', { responseType: 'text' }).pipe(
-        switchMap((data: string) => this.httpClient.get<Record<string, string>>(`https://ipapi.co/${data}/json/`)),
-        // https://api.apicagent.com/?ua=Mozilla/5.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_15_5)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/89.0.4389.114%20Safari/537.36
-        map((ip: Record<string, string>) => setIpAction({ ip }))
-      ))
+      switchMap((action: TypedAction<"[Imprint Resolver] Load IP">) => this.httpClient.get('https://checkip.amazonaws.com/', { responseType: 'text' })),
+      map((data: string) => this.httpClient.get<Record<string, string>>(`https://ipapi.co/${data}/json/`)),
+      switchMap((res$: Observable<Record<string, string>>) => combineLatest<[Record<string, string>, string]>([res$, this.httpClient.get('https://1.1.1.1/cdn-cgi/trace',{ responseType: 'text' }).pipe(map((res: string) => this.parseConfig<'uag'>(res).uag))])),
+      map((res: [Record<string, string>, string]) => assoc<string | Record<string, string> | undefined, Record<string, string>, 'user'>('user', last<string | Record<string, string>>(res), head<Record<string, string>, string>(res))),
+      map((ip: Record<string, string>) => setIpAction({ ip })),
     );
   }, { dispatch: true });
 
@@ -37,5 +39,21 @@ export class MainEffects {
       // console.log(action.type);
       // this.httpClient.get('http://localhost:3000/api').subscribe((data: any) => {});
     });
+
+    this.setTechnologyDisabledEffect$.pipe(take(1));
+    this.setImprintIpEffect$.pipe(take(1));
+  }
+
+  private parseConfig<T extends string>(inputString: string): Record<T, string> {
+    const lines: Array<string> = inputString.split('\n');
+    const configObject: Record<string, string> = {};
+
+    lines.forEach((line: string): void => {
+      const [key, value]: Array<string> = line.split('=');
+
+      configObject[key] = value;
+    });
+
+    return configObject;
   }
 }
